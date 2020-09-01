772d93151cce4956dfd746553e599d94d4ad2802
brhwang0 <brhwang@protonmail.com>
2020-09-01 19:56:18 -0400

    Add fetchData call to App

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import React, { Component } from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
+import { fetchData } from "./api";
 
 class App extends Component {
   state = {};
+
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+
   render() {
     return (
       <div className={styles.container}>
