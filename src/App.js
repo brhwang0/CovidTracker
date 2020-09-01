a43a8227b7320ee3c2c37e04f6d2d6d309ea8412
brhwang0 <brhwang@protonmail.com>
2020-09-01 19:46:14 -0400

    Import components to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 
+import { Cards, Chart, CountryPicker } from "./components";
+
 class App extends Component {
   state = {};
   render() {
     return (
       <div>
-        <h1>App</h1>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
