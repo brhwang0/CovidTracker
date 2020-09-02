23631e017c5c54dca998f9276b760bca7e72c804
brhwang0 <brhwang@protonmail.com>
2020-09-02 01:19:54 -0400

    Add placeholder card components

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,7 +1,53 @@
 import React, { Component } from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+
+import styles from "./Cards.module.css";
 
 const Cards = (props) => {
-  return <h1> Cards</h1>;
+  return (
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">Data Placeholder</Typography>
+            <Typography color="textSecondary">Date Placeholder</Typography>
+            <Typography variant="body2">
+              Number of active cases of COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Recovered
+            </Typography>
+            <Typography variant="h5">Data Placeholder</Typography>
+            <Typography color="textSecondary">Date Placeholder</Typography>
+            <Typography variant="body2">
+              Number of active cases of COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography variant="h5">Data Placeholder</Typography>
+            <Typography color="textSecondary">Date Placeholder</Typography>
+            <Typography variant="body2">
+              Number of active cases of COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Cards;
