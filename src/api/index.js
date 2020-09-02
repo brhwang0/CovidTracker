f07e18be400373d539e5576e3b15ccf66a589af1
brhwang0 <brhwang@protonmail.com>
2020-09-01 20:40:05 -0400

    Update API to filter data

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,11 @@ const url = "https://covid19.mathdro.id/api";
 export const fetchData = async () => {
   // try will be executed if fetch is successful
   try {
-    const response = await axios.get(url);
-    return response;
-  } catch (error) {}
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {
+    console.log(error);
+  }
 };
